55ee17902ad42b85e2f25d1cd287c9ad9127a7db
Logan Croley <logan.croley.18@gmail.com>
2026-01-08 08:22:36 -0500

    added reroll button

--- data/player/player_stats.gd
+++ data/player/player_stats.gd
@@ -1,9 +1,25 @@
 class_name PlayerStats
 extends Resource
 
+const XP_REQUIREMENTS := {
+	1: 0,
+	2: 2,
+	3: 2,
+	4: 6,
+	5: 10,
+	6: 20,
+	7: 36,
+	8: 48,
+	9: 76,
+	10: 76
+}
+
 @export_range(0,99) var gold: int: set = _set_gold
 @export_range(0,99) var xp: int: set = _set_xp
 @export_range(1,10) var level: int: set = _set_level
+
+func get_current_xp_requirement() -> int:
+	return XP_REQUIREMENTS[level+1]
 
 func _set_gold(value: int) -> void:
 	gold = value
