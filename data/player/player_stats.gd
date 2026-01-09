3b07d3dc8b9364d432b2539b4ec4d1986cb055d7
Logan Croley <logan.croley.18@gmail.com>
2026-01-08 22:58:46 -0500

    middle of xp_tracker - has a bug :(

--- data/player/player_stats.gd
+++ data/player/player_stats.gd
@@ -24,11 +24,21 @@ func get_current_xp_requirement() -> int:
 func _set_gold(value: int) -> void:
 	gold = value
 	emit_changed()
-	
+
 func _set_xp(value: int) -> void:
 	xp = value
-	emit_changed()
+	if level == 10:
+		return
+	var xp_requirement: int = get_current_xp_requirement()
+	while xp >= xp_requirement:
+		level += 1
+		xp -= xp_requirement
+		xp_requirement = get_current_xp_requirement()
+		emit_changed()
 	
 func _set_level(value: int) -> void:
 	level = value
 	emit_changed()
+
+func is_max_level() -> bool:
+	return  level ==10
