da8261e7e586d8cb60dbba865cb12c9512302e3f
Logan Croley <logan.croley.18@gmail.com>
2026-01-14 21:34:37 -0500

    connected stuff

--- scenes/arena/arena.gd
+++ scenes/arena/arena.gd
@@ -8,7 +8,9 @@ const QUARTER_CELL_SIZE := Vector2(16,16)
 @onready var unit_mover: UnitMover = $UnitMover
 @onready var unit_spawner: UnitSpawner = $UnitSpawner
 @onready var sell_portal: SellPortal = $SellPortal
+@onready var shop: Shop = %Shop
 
 func _ready() -> void:
 	unit_spawner.unit_spawned.connect(unit_mover.setup_unit)
 	unit_spawner.unit_spawned.connect(sell_portal.setup_unit)
+	shop.unit_bought.connect(unit_spawner.spawn_unit)
